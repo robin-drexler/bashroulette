a8d95491b70322066ca45de567c4fe239afe0dc7
Robin Drexler <drexler.robin@gmail.com>
2017-02-24 20:44:39 +0100

    feat: proof of concept :rocket:

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var server;
+
+app.all('/pullthetrigger', function (req, res) {
+  const commands = [
+    'ls',
+    'cat /etc/passwd',
+    'pwd',
+    'git status',
+    'cat ~/.bashrc',
+    'echo "lol"'
+  ];
+  const commandToRun = commands[Math.floor(Math.random() * commands.length)];
+
+  res.send(commandToRun);
+});
+
+
+exports.start = function(cb) {
+  server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    if (cb) {
+      cb();
+    }
+    console.log('Example app listening at http://%s:%s', host, port);
+  });
+}
+
+exports.close = function(cb) {
+  if (server) {
+    server.close(cb);
+  } else {
+    cb();
+  }
+}
+
+if (module.id === require.main.id) {
+  exports.start()
+}
